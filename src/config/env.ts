bac63fc45020bd2662e292f057e9b974ef33d3f8
nuza555xx <nuchaiwongthon@gmail.com>
2026-03-17 15:51:18 +0700

    chore: remove port

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,9 +1,9 @@
 import { config } from 'dotenv';
 
-config({ override: true });
+config();
 
 export const env = {
-  port: parseInt(process.env.PORT || '3000', 10),
+  port: parseInt(process.env.PORT || '8080', 10),
   supabaseUrl: process.env.SUPABASE_URL || '',
   supabaseAnonKey: process.env.SUPABASE_ANON_KEY || '',
   supabaseServiceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY || '',
